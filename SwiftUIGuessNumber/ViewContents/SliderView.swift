c4964cfae09d37b10e9d45c27cf087f82a87e76f
kuznet.i.sova <kis7425910@gmail.com>
2020-07-31 23:17:49 +0300

    added structure

---
+++ SwiftUIGuessNumber/ViewContents/SliderView.swift
@@ -0,0 +1,21 @@
+//
+//  SliderView.swift
+//  SwiftUIGuessNumber
+//
+//  Created by Ирина Кузнецова on 31.07.2020.
+//  Copyright © 2020 Irina Kuznetsova. All rights reserved.
+//
+
+import SwiftUI
+
+struct SliderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SliderView_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderView()
+    }
+}
