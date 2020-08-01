84bd1491ba4a2f50452c42321793b5a261240578
kuznet.i.sova <kis7425910@gmail.com>
2020-08-01 16:48:36 +0300

    add alpha for slider

--- SwiftUIGuessNumber/ViewContents/UiKitSlider.swift
+++ SwiftUIGuessNumber/ViewContents/UiKitSlider.swift
@@ -10,12 +10,14 @@ import SwiftUI
 
 struct UiKitSlider: UIViewRepresentable {
     @Binding var value: Double
+    @Binding var alpha: CGFloat
     
     func makeUIView(context: Context) -> UISlider {
         let slider = UISlider(frame: .zero)
         slider.minimumValue = 0
         slider.maximumValue = 255
         slider.value = Float(value)
+        slider.alpha = alpha
         
         slider.addTarget(context.coordinator,
                          action: #selector(Coordinator.valueChanged),
@@ -48,6 +50,6 @@ extension UiKitSlider {
 
 struct UiKitSlider_Previews: PreviewProvider {
     static var previews: some View {
-        UiKitSlider(value: .constant(100))
+        UiKitSlider(value: .constant(100), alpha: .constant(0.3))
     }
 }
