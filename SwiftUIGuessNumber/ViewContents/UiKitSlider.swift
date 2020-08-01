80946786b8c5d744368d7b4ff7694103a84ab79c
kuznet.i.sova <kis7425910@gmail.com>
2020-08-01 16:43:45 +0300

    added UiKitSlider and delete SliderViewController+Main.storyboard

---
+++ SwiftUIGuessNumber/ViewContents/UiKitSlider.swift
@@ -0,0 +1,53 @@
+//
+//  UiKitSlider.swift
+//  SwiftUIGuessNumber
+//
+//  Created by Ирина Кузнецова on 01.08.2020.
+//  Copyright © 2020 Irina Kuznetsova. All rights reserved.
+//
+
+import SwiftUI
+
+struct UiKitSlider: UIViewRepresentable {
+    @Binding var value: Double
+    
+    func makeUIView(context: Context) -> UISlider {
+        let slider = UISlider(frame: .zero)
+        slider.minimumValue = 0
+        slider.maximumValue = 255
+        slider.value = Float(value)
+        
+        slider.addTarget(context.coordinator,
+                         action: #selector(Coordinator.valueChanged),
+                         for: .valueChanged)
+        return slider
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        uiView.value = Float(value)
+    }
+    
+    func makeCoordinator() -> UiKitSlider.Coordinator {
+        Coordinator(value: $value)
+    }
+}
+
+extension UiKitSlider {
+    class Coordinator: NSObject {
+        @Binding var value: Double
+        
+        init(value: Binding<Double>) {
+            self._value = value
+        }
+        
+        @objc func valueChanged(_ sender: UISlider) {
+            value = Double(sender.value)
+        }
+    }
+}
+
+struct UiKitSlider_Previews: PreviewProvider {
+    static var previews: some View {
+        UiKitSlider(value: .constant(100))
+    }
+}
